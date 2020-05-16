f5461a406d8cb33a815dcf5262cbc7ef24974b66
Aman Puri <46370690+Aman13Puri@users.noreply.github.com>
2020-05-16 20:20:51 +0530

    Add files via upload

---
+++ webcam_cv3.py
@@ -0,0 +1,49 @@
+import cv2
+
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)
+anterior = 0
+
+while True:
+    if not video_capture.isOpened():
+        print('Unable to load camera.')
+        sleep(5)
+        pass
+
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+    if faces != ():
+        print(faces)
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    if anterior != len(faces):
+        anterior = len(faces)
+
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
